c0699462c4c210c335e186ce62f5c7f8a8f7e617
konglinghui <17610760836@163.com>
2020-04-18 22:24:29 +0800

    记账系统脚本修改

--- sql/记账系统.sql
+++ sql/记账系统.sql
@@ -8,7 +8,7 @@ CREATE TABLE account_category (
     icon        varchar(32) NOT NULL COMMENT '图标',
     order_no    int(10)     NOT NULL DEFAULT 0 COMMENT '排序号',
     status      tinyint(2)  NOT NULL DEFAULT 0 COMMENT '状态: 0-启用 1-禁用',
-    description varchar(32) NULL     DEFAULT '' COMMENT '描述',
+    remark      varchar(32) NULL     DEFAULT '' COMMENT '备注',
     create_by   varchar(32) NOT NULL COMMENT '创建人ID',
     create_time datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_by   varchar(32) NOT NULL COMMENT '更新人ID',
@@ -18,7 +18,7 @@ CREATE TABLE account_category (
 )
     ENGINE = InnoDB
     CHARACTER SET = utf8
-    COMMENT = '账单分类表';
+    COMMENT = '消费分类表';
 
 -- 账单记录表
 DROP TABLE IF EXISTS account_record;
@@ -26,7 +26,8 @@ CREATE TABLE account_record (
     id          bigint(20)     NOT NULL COMMENT '主键',
     person_id   bigint(20)     NOT NULL COMMENT '用户主键',
     category_id bigint(20)     NOT NULL COMMENT '账单分类主键',
-    amount      decimal(10, 2) NOT NULL COMMENT '金额: 包括收入、支出金额；单位-分',
+    type        tinyint(2)     NOT NULL COMMENT '收支类型: 1-收入，2-支出',
+    amount      decimal(10, 2) NOT NULL COMMENT '金额: 单位(分)',
     remark      varchar(32)    NULL     DEFAULT '' COMMENT '备注',
     create_by   varchar(32)    NOT NULL COMMENT '创建人ID',
     create_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -36,4 +37,4 @@ CREATE TABLE account_record (
 )
     ENGINE = InnoDB
     CHARACTER SET = utf8
-    COMMENT = '账单记录表';
+    COMMENT = '消费记录表';
