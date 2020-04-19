f5d2d19fbecad095bea52486df13ca10b8677b7d
konglinghui <17610760836@163.com>
2020-04-19 11:54:45 +0800

    添加实体类相关注解属性

--- dashboard-common/src/main/java/com/dashboard/entity/account/AccountCategory.java
+++ dashboard-common/src/main/java/com/dashboard/entity/account/AccountCategory.java
@@ -1,9 +1,14 @@
 package com.dashboard.entity.account;
 
 import com.dashboard.common.entity.BaseDO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
@@ -12,7 +17,9 @@ import java.io.Serializable;
  * @date 2020/4/18 21:35
  **/
 @Data
+@ApiModel
 @EqualsAndHashCode(callSuper = false)
+@Table(name = "account_category")
 public class AccountCategory extends BaseDO implements Serializable {
 
     private static final long serialVersionUID = 5274555393847892683L;
@@ -20,40 +27,57 @@ public class AccountCategory extends BaseDO implements Serializable {
     /**
      * 主键ID
      */
+    @Id
+    @Column(name = "id")
+    @ApiModelProperty("主键ID")
     private Long id;
 
     /**
      * 上级主键ID
      */
+    @Column(name = "parent_id")
+    @ApiModelProperty("上级主键ID")
     private Long parentId;
 
     /**
      * 分类名称
      */
+    @Column(name = "name")
+    @ApiModelProperty("分类名称")
     private String name;
 
     /**
      * 类型: 1-一级分类 2-二级分类 3-三级分类
      */
+    @Column(name = "type")
+    @ApiModelProperty("类型: 1-一级分类 2-二级分类 3-三级分类")
     private Integer type;
 
     /**
      * 图标
      */
+    @Column(name = "icon")
+    @ApiModelProperty("图标")
     private String icon;
 
     /**
      * 排序号
      */
+    @Column(name = "order_no")
+    @ApiModelProperty("排序号")
     private Integer orderNo;
 
     /**
      * 状态: 0-启用 1-禁用
      */
+    @Column(name = "status")
+    @ApiModelProperty("状态: 0-启用 1-禁用")
     private Integer status;
 
     /**
      * 备注
      */
+    @Column(name = "remark")
+    @ApiModelProperty("备注")
     private String remark;
 }
