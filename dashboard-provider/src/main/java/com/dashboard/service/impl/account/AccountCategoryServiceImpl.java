f03a239acae34f91b4c7ae77d0aeb0e9665f88a2
konglinghui <17610760836@163.com>
2020-04-25 21:46:25 +0800

    消费分类树结构服务修改

--- dashboard-provider/src/main/java/com/dashboard/service/impl/account/AccountCategoryServiceImpl.java
+++ dashboard-provider/src/main/java/com/dashboard/service/impl/account/AccountCategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.dashboard.service.impl.account;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.dashboard.common.constants.DashboardConstants;
 import com.dashboard.common.entity.Page;
+import com.dashboard.common.enums.StatusEnum;
 import com.dashboard.entity.account.AccountCategory;
 import com.dashboard.entity.account.AccountCategoryPageInfo;
 import com.dashboard.entity.account.AccountCategoryTreeVO;
@@ -12,9 +13,14 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author konglinghui
@@ -57,10 +63,45 @@ public class AccountCategoryServiceImpl implements AccountCategoryService {
         List<AccountCategoryTreeVO> accountCategoryTreeVOList = new ArrayList<>();
 
         // 根节点
-        AccountCategoryTreeVO accountCategoryTreeVO = new AccountCategoryTreeVO();
-        accountCategoryTreeVO.setId(DashboardConstants.ZERO_LONG.toString());
-        accountCategoryTreeVO.setName("根节点");
-        accountCategoryTreeVOList.add(accountCategoryTreeVO);
+        AccountCategoryTreeVO rootAccountCategoryTreeVO = new AccountCategoryTreeVO();
+        rootAccountCategoryTreeVO.setId(DashboardConstants.ZERO_LONG);
+        rootAccountCategoryTreeVO.setName("根节点");
+        accountCategoryTreeVOList.add(rootAccountCategoryTreeVO);
+
+        Condition condition = new Condition(AccountCategory.class);
+        Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("status", StatusEnum.ON.getCode());
+        List<AccountCategory> accountCategoryList = accountCategoryMapper.selectByCondition(condition);
+
+        Map<Long, AccountCategoryTreeVO> treeVOMap =
+                accountCategoryList.stream()
+                        .collect(
+                                Collectors.toMap(AccountCategory::getId,
+                                        accountCategory -> {
+                                            AccountCategoryTreeVO accountCategoryTreeVO = new AccountCategoryTreeVO();
+                                            accountCategoryTreeVO.setId(accountCategory.getId());
+                                            accountCategoryTreeVO.setName(accountCategory.getName());
+                                            accountCategoryTreeVO.setParentId(accountCategory.getParentId());
+                                            accountCategoryTreeVO.setChildren(new ArrayList<>());
+
+                                            return accountCategoryTreeVO;
+                                        }));
+
+        accountCategoryList.forEach(accountCategory -> {
+            Long id = accountCategory.getId();
+            Long parentId = accountCategory.getParentId();
+            AccountCategoryTreeVO accountCategoryTreeVO = treeVOMap.get(id);
+            if (DashboardConstants.ZERO_LONG.equals(parentId) && Objects.nonNull(accountCategoryTreeVO)) {
+                // 一级分类
+                accountCategoryTreeVOList.add(accountCategoryTreeVO);
+            } else {
+                // 子分类通过parentID获取到父分类
+                AccountCategoryTreeVO parentAccountCategoryTreeVO = treeVOMap.get(parentId);
+                if (Objects.nonNull(parentAccountCategoryTreeVO)) {
+                    parentAccountCategoryTreeVO.getChildren().add(accountCategoryTreeVO);
+                }
+            }
+        });
 
         return accountCategoryTreeVOList;
     }
