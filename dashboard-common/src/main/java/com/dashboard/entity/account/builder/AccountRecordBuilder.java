2ad61ed877e0d7ffda201566caad088b049bd5ac
konglinghui <17610760836@163.com>
2020-04-30 17:21:44 +0800

    记录实体构建器

---
+++ dashboard-common/src/main/java/com/dashboard/entity/account/builder/AccountRecordBuilder.java
@@ -0,0 +1,54 @@
+package com.dashboard.entity.account.builder;
+
+import com.dashboard.entity.account.AccountRecord;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author konglinghui
+ * @description 记录构建器【建造者模式】
+ * @date 2020/4/30 16:54
+ **/
+public class AccountRecordBuilder implements Serializable {
+
+    private static final long serialVersionUID = -2349668467483357754L;
+
+    /**
+     * 记录
+     */
+    private AccountRecord target;
+
+    public AccountRecordBuilder() {
+        this.target = new AccountRecord();
+    }
+
+    public AccountRecordBuilder id(Long id) {
+        this.target.setId(id);
+        return this;
+    }
+
+    public AccountRecordBuilder personId(String personId) {
+        this.target.setPersonId(personId);
+        return this;
+    }
+
+    public AccountRecordBuilder categoryId(Long categoryId) {
+        this.target.setCategoryId(categoryId);
+        return this;
+    }
+
+    public AccountRecordBuilder amount(BigDecimal amount) {
+        this.target.setAmount(amount);
+        return this;
+    }
+
+    public AccountRecordBuilder remark(String remark) {
+        this.target.setRemark(remark);
+        return this;
+    }
+
+    public AccountRecord build() {
+        return this.target;
+    }
+}
