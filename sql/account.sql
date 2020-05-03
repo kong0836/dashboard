e39a9e32a1b2785b7877015999bdce0cf5acb0f9
konglinghui <17610760836@163.com>
2020-05-03 21:16:56 +0800

    sql脚本修改

--- sql/account.sql
+++ sql/account.sql
@@ -1,5 +1,5 @@
 -- 账单分类表
-DROP TABLE IF EXISTS account_category;
+# DROP TABLE IF EXISTS account_category;
 CREATE TABLE account_category (
     id          bigint(20)  NOT NULL COMMENT '主键',
     parent_id   bigint(20)  NOT NULL COMMENT '上级分类主键',
@@ -20,19 +20,21 @@ CREATE TABLE account_category (
     CHARACTER SET = utf8
     COMMENT = '消费分类表';
 
--- 账单记录表
+-- 消费记录表
 DROP TABLE IF EXISTS account_record;
 CREATE TABLE account_record (
-    id          bigint(20)     NOT NULL COMMENT '主键',
-    person_id   bigint(20)     NOT NULL COMMENT '用户主键',
-    category_id bigint(20)     NOT NULL COMMENT '账单分类主键',
-    amount      decimal(10, 2) NOT NULL COMMENT '金额: 单位(分)',
-    status      tinyint(2)     NOT NULL DEFAULT 0 COMMENT '状态: 0-启用 1-禁用',
-    remark      varchar(32)    NULL     DEFAULT '' COMMENT '备注',
-    create_by   varchar(32)    NOT NULL COMMENT '创建人ID',
-    create_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_by   varchar(32)    NOT NULL COMMENT '更新人ID',
-    update_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    id            bigint(20)     NOT NULL COMMENT '主键',
+    person_id     bigint(20)     NOT NULL COMMENT '用户主键',
+    category_id   bigint(20)     NOT NULL COMMENT '账单分类主键',
+    consumer_date date           NOT NULL COMMENT '消费日期：默认当天',
+    type          tinyint(2)     NOT NULL COMMENT '消费类型: 1-收入 2-支出',
+    amount        decimal(10, 2) NOT NULL COMMENT '金额: 单位(分)',
+    status        tinyint(2)     NOT NULL DEFAULT 0 COMMENT '状态: 0-启用 1-禁用',
+    remark        varchar(32)    NULL     DEFAULT '' COMMENT '备注',
+    create_by     varchar(32)    NOT NULL COMMENT '创建人ID',
+    create_time   datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_by     varchar(32)    NOT NULL COMMENT '更新人ID',
+    update_time   datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
     PRIMARY KEY `account_category_record_id` (id)
 )
     ENGINE = InnoDB
