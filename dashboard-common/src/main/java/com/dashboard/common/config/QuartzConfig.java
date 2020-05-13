3563039a935cc5c78b6072c6e2d9c5f570a67e85
konglinghui <17610760836@163.com>
2020-05-13 20:15:51 +0800

    【定时任务】-配置文件

---
+++ dashboard-common/src/main/java/com/dashboard/common/config/QuartzConfig.java
@@ -0,0 +1,12 @@
+package com.dashboard.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author konglinghui
+ * @description 定时任务配置
+ * @date 2020/5/13 12:26
+ **/
+@Configuration
+public class QuartzConfig {
+}
