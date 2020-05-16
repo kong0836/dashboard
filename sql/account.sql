cc938f89453bcca6a14b4be271c2b5281dbfa557
konglinghui <17610760836@163.com>
2020-05-16 18:51:29 +0800

    【消费记录汇总表】

--- sql/account.sql
+++ sql/account.sql
@@ -1,5 +1,6 @@
 -- 消费分类表
 # DROP TABLE IF EXISTS account_category;
+
 CREATE TABLE account_category (
     id          bigint(20)  NOT NULL COMMENT '主键',
     parent_id   bigint(20)  NOT NULL COMMENT '上级分类主键',
@@ -22,6 +23,7 @@ CREATE TABLE account_category (
 
 -- 消费记录表
 DROP TABLE IF EXISTS account_record;
+
 CREATE TABLE account_record (
     id            bigint(20)     NOT NULL COMMENT '主键',
     person_id     bigint(20)     NOT NULL COMMENT '用户主键',
@@ -45,6 +47,7 @@ CREATE TABLE account_record (
 
 -- 消费预算表
 DROP TABLE IF EXISTS account_budget;
+
 CREATE TABLE account_budget (
     id          bigint(20)     NOT NULL COMMENT '主键',
     category_id bigint(20)     NOT NULL COMMENT '消费分类主键',
@@ -61,3 +64,23 @@ CREATE TABLE account_budget (
     ENGINE = InnoDB
     CHARACTER SET = utf8
     COMMENT = '消费预算表';
+
+-- 消费记录汇总表
+DROP TABLE IF EXISTS account_total;
+
+CREATE TABLE account_total (
+    id          bigint(20)     NOT NULL COMMENT '主键',
+    category_id bigint(20)     NOT NULL COMMENT '消费分类主键',
+    type        tinyint(2)     NOT NULL COMMENT '汇总方式: 日、周、月、季度、半年、年',
+    amount      decimal(10, 2) NOT NULL COMMENT '预算金额: 单位(分)',
+    status      tinyint(2)     NOT NULL DEFAULT 0 COMMENT '状态: 0-启用 1-禁用',
+    remark      varchar(32)    NULL     DEFAULT '' COMMENT '备注',
+    create_by   varchar(32)    NOT NULL COMMENT '创建人ID',
+    create_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_by   varchar(32)    NOT NULL COMMENT '更新人ID',
+    update_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    PRIMARY KEY `account_total_id` (id)
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8
+    COMMENT = '消费记录汇总表';
