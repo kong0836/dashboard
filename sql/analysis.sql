aafea0f601b0c52746155c400952cd6807564d31
konglinghui <17610760836@163.com>
2020-05-22 20:19:18 +0800

    【sql脚本】

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,1 +1,20 @@
-# 统计分析
+-- 消费记录汇总表
+DROP TABLE IF EXISTS analysis_account_total;
+
+CREATE TABLE analysis_account_total (
+    id          bigint(20)     NOT NULL COMMENT '主键',
+    person_id   bigint(20)     NOT NULL COMMENT '用户主键',
+    category_id bigint(20)     NOT NULL COMMENT '消费分类主键',
+    type        tinyint(2)     NOT NULL COMMENT '汇总方式: 日、周、月、季度、半年、年',
+    amount      decimal(10, 2) NOT NULL COMMENT '金额: 单位(分)',
+    status      tinyint(2)     NOT NULL DEFAULT 0 COMMENT '状态: 0-启用 1-禁用',
+    remark      varchar(32)    NULL     DEFAULT '' COMMENT '备注',
+    create_by   varchar(32)    NOT NULL COMMENT '创建人ID',
+    create_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_by   varchar(32)    NOT NULL COMMENT '更新人ID',
+    update_time datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    PRIMARY KEY `account_total_id` (id)
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8
+    COMMENT = '消费记录汇总表';
