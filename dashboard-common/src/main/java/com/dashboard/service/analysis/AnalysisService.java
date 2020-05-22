37823956a94f0d0360788f9b18f8e3cc1aa5c572
konglinghui <17610760836@163.com>
2020-05-22 19:58:46 +0800

    【统计分析】-服务

---
+++ dashboard-common/src/main/java/com/dashboard/service/analysis/AnalysisService.java
@@ -0,0 +1,9 @@
+package com.dashboard.service.analysis;
+
+/**
+ * @author konglinghui
+ * @description 统计分析服务
+ * @date 2020/5/22 19:13
+ **/
+public interface AnalysisService {
+}
---
+++ dashboard-provider/src/main/java/com/dashboard/service/impl/analysis/AnalysisServiceImpl.java
@@ -0,0 +1,13 @@
+package com.dashboard.service.impl.analysis;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.dashboard.service.analysis.AnalysisService;
+
+/**
+ * @author konglinghui
+ * @description 统计分析服务
+ * @date 2020/5/22 19:13
+ **/
+@Service
+public class AnalysisServiceImpl implements AnalysisService {
+}
